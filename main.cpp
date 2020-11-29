f5ddf47a2ca54a00ea25b38d650a9eda0774e598
Michal <minto.j2000@gmail.com>
2020-11-29 23:08:57 +0100

    Finished: #3 #4 #5 #6

--- main.cpp
+++ main.cpp
@@ -3,19 +3,71 @@
 #include <fstream>
 #include <sstream>
 
-void linesAmount(std::string file){
+void linesAmount(std::string file) {
+    std::fstream input_file(file);
+    int counter = 0;
+    for (std::string line; std::getline(input_file, line);) {
+        std::stringstream stream(line);
+        ++counter;
+    }
+    std::cout << "Amount of lines in specified file: " << counter << '\n';
+}
+
+void numberAmount(std::string file) {
+    std::fstream input_file(file);
+    int counter = 0;
+    for (std::string line; std::getline(input_file, line);) {
+        std::stringstream stream(line);
+        for (std::string s; stream >> s;) {
+            std::string::const_iterator it = s.begin();
+            while (it != s.end() && std::isdigit(*it)) ++it;
+            if (!s.empty() && s.find_first_not_of("-0123456789") == std::string::npos) {
+                ++counter;
+            }
+        }
+    }
+    std::cout << "Numbers in specified file: " << counter << '\n';
+}
+
+
+void digitAmount(std::string file) {
+    std::fstream input_file(file);
+    int counter = 0;
+    for (std::string line; std::getline(input_file, line);) {
+        std::stringstream stream(line);
+        for (std::string s; stream >> s;) {
+            for (int i = 0; i < s.size(); ++i) {
+                if (isdigit(s[i])) {
+                    counter++;
+                }
+            }
+        }
+    }
+    std::cout << "Digits in specified file: " << counter << '\n';
+
+}
+
+void charAmount(std::string file) {
     std::fstream input_file(file);
     int counter = 0;
     for (std::string line; std::getline(input_file, line); ) {
         std::stringstream stream(line);
-        ++counter;
+        for (int i = 0; i < line.length(); ++i) {
+            if (isspace(line.at(i))){
+                ++counter;
+            }
+        }
+        for (std::string word; stream >> word; ) {
+            counter = counter + word.size();
+        }
     }
-    std::cout << counter;
+    std::cout << "Number of chars in specified file: " << counter << '\n';
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::string file = "test.txt";
-    linesAmount("C:\\Users\\minto.MSI-B450TM\\CLionProjects\\PJAText\\test.txt");
-    return 0;
+    std::string file = R"(C:\Users\minto.MSI-B450TM\CLionProjects\PJAText\test.txt)";
+    linesAmount(file);
+    numberAmount(file);
+    digitAmount(file);
+    charAmount(file);
 }

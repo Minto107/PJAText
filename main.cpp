7cc2db9da83ce26af8234998a05a58caecb57815
Michal <minto.j2000@gmail.com>
2020-11-30 14:41:56 +0100

    #8 done

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 void linesAmount(std::string file) {
     std::fstream input_file(file);
@@ -86,9 +87,6 @@ void find_anagrams_backup(std::string file, std::string anagrams_to_find) {
             for (auto c : s) {
                 occurrences[c] += 1;
             }
-            /*for (auto[k, value] : occurrences) {
-                std::cout << k << " - " << value << '\n';
-            }*/
             std::string tmp;
             std::vector<std::string> first;
             std::string tmp1;
@@ -98,14 +96,12 @@ void find_anagrams_backup(std::string file, std::string anagrams_to_find) {
                 oss << key << " + " << value;
                 tmp = oss.str();
                 first.push_back(tmp);
-                //std::cout << tmp << '\n';
             }
             for (auto[key, value] : occurrences) {
                 std::ostringstream oss;
                 oss << key << " + " << value;
                 tmp1 = oss.str();
                 second.push_back(tmp1);
-                //std::cout << tmp1 << '\n';
             }
             if (first == second) {
                 std::cout << "Word " << s << " is an anagram to " << anagrams_to_find << '\n';
@@ -119,15 +115,9 @@ void find_anagrams(std::string file, std::vector<std::string> anagrams_to_find) 
     for (int i = 0; i < anagrams_to_find.size(); ++i) {
         std::string anagram_to_find = anagrams_to_find[i];
         std::map<char, int> occurrences_in_anagrams_to_find;
-
         for (auto c : anagram_to_find) {
             occurrences_in_anagrams_to_find[c] += 1;
         }
-        //std::cout << "Word: " << anagram_to_find << '\n';
-//        for (auto[k, value] : occurrences_in_anagrams_to_find) {
-//            std::cout << k << " - " << value << '\n';
-//        }
-        //std::cout << "Anagram size: " << anagram_to_find.length() << '\n';
         std::cout << "Anagrams for word " << anagram_to_find << ": ";
         std::fstream input_file(file);
         for (std::string line; std::getline(input_file, line);) {
@@ -138,9 +128,6 @@ void find_anagrams(std::string file, std::vector<std::string> anagrams_to_find) 
                 for (auto c : s) {
                     occurrences[c] += 1;
                 }
-                /*for (auto[k, value] : occurrences) {
-                    std::cout << k << " - " << value << '\n';
-                }*/
                 std::string tmp;
                 std::vector<std::string> first;
                 std::string tmp1;
@@ -150,14 +137,12 @@ void find_anagrams(std::string file, std::vector<std::string> anagrams_to_find) 
                     oss << key << " + " << value;
                     tmp = oss.str();
                     first.push_back(tmp);
-                    //std::cout << tmp << '\n';
                 }
                 for (auto[key, value] : occurrences) {
                     std::ostringstream oss;
                     oss << key << " + " << value;
                     tmp1 = oss.str();
                     second.push_back(tmp1);
-                    //std::cout << tmp1 << '\n';
                 }
                 if (first == second) {
                     std::cout << s << ", ";
@@ -166,7 +151,27 @@ void find_anagrams(std::string file, std::vector<std::string> anagrams_to_find) 
         }
         std::cout << '\n';
     }
+}
 
+void find_palindrome(std::string file, std::vector<std::string> palindromes) {
+    for (int i = 0; i < palindromes.size(); ++i)
+    {
+        std::string palindrome = palindromes[i];
+        std::fstream input_file(file);
+        std::cout << "Palindromes for word " << palindrome << ": ";
+        std::reverse(palindrome.begin(), palindrome.end());
+        for (std::string line; std::getline(input_file, line);) {
+            std::stringstream stream(line);
+            for (std::string word; stream >> word;) {
+                std::string tmp = word;
+                std::reverse(tmp.begin(), tmp.end());
+                if (tmp == palindrome) {
+                    std::cout << tmp << ", ";
+                }
+            }
+        }
+        std::cout << '\n';
+    }
 }
 
 int main() {
@@ -181,4 +186,9 @@ int main() {
     anagram_v.emplace_back("pap");
     anagram_v.emplace_back("life");
     find_anagrams(file, anagram_v);
+    std::string palindrome = "test";
+    std::vector<std::string> palindromes;
+    palindromes.push_back(palindrome);
+    palindromes.emplace_back("there");
+    find_palindrome(file, palindromes);
 }
